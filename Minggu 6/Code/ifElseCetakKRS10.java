c2d4d1b2ffd08474c7be368ae8bda69e2cd8383d
galihcandraa <galihcandrakirana17@gmail.com>
2025-09-30 22:30:25 +0700

    Tugas 1

--- Minggu 6/Code/ifElseCetakKRS10.java
+++ Minggu 6/Code/ifElseCetakKRS10.java
@@ -9,25 +9,36 @@ public class ifElseCetakKRS10 {
         System.out.print("Masukkan semester saat ini: ");
         int semester = sc.nextInt();
 
-        if (semester == 1) {
-            System.out.println("KRS Semester 1 ditampilkan");
-        } else if (semester == 2) {
-            System.out.println("KRS Semester 2 ditampilkan");
-        } else if (semester == 3) {
-            System.out.println("KRS Semester 3 ditampilkan");
-        } else if (semester == 4) {
-            System.out.println("KRS Semester 4 ditampilkan");
-        } else if (semester == 5) {
-            System.out.println("KRS Semester 5 ditampilkan");
-        } else if (semester == 6) {
-            System.out.println("KRS Semester 6 ditampilkan");
-        } else if (semester == 7) {
-            System.out.println("KRS Semester 7 ditampilkan");
-        } else if (semester == 8) {
-            System.out.println("KRS Semester 8 ditampilkan");
-        } else {
-            System.out.println("Semester tidak valid");
-        }
+        // if (semester == 1) {
+        //     System.out.println("KRS Semester 1 ditampilkan");
+        // } else if (semester == 2) {
+        //     System.out.println("KRS Semester 2 ditampilkan");
+        // } else if (semester == 3) {
+        //     System.out.println("KRS Semester 3 ditampilkan");
+        // } else if (semester == 4) {
+        //     System.out.println("KRS Semester 4 ditampilkan");
+        // } else if (semester == 5) {
+        //     System.out.println("KRS Semester 5 ditampilkan");
+        // } else if (semester == 6) {
+        //     System.out.println("KRS Semester 6 ditampilkan");
+        // } else if (semester == 7) {
+        //     System.out.println("KRS Semester 7 ditampilkan");
+        // } else if (semester == 8) {
+        //     System.out.println("KRS Semester 8 ditampilkan");
+        // } else {
+        //     System.out.println("Semester tidak valid");
+        // }
+
+        String pesan;
+        pesan = (semester == 1) ? "KRS Semester 1 ditampilkan" : 
+                (semester == 2) ? "KRS Semester 2 ditampilkan" :
+                (semester == 3) ? "KRS Semester 3 ditampilkan" :
+                (semester == 4) ? "KRS Semester 4 ditampilkan" :
+                (semester == 5) ? "KRS Semester 5 ditampilkan" :
+                (semester == 6) ? "KRS Semester 6 ditampilkan" :
+                (semester == 7) ? "KRS Semester 7 ditampilkan" :
+                (semester == 8) ? "KRS Semester 8 ditampilkan" : "Semester tidak valid";
+        System.out.println(pesan);
         sc.close();
     }
 }
