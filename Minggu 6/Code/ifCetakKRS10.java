1c1971186b4b9c04c35730dba8bacc41cf291fa5
galihcandraa <galihcandrakirana17@gmail.com>
2025-09-30 13:58:08 +0700

    Percobaan 1

---
+++ Minggu 6/Code/ifCetakKRS10.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ifCetakKRS10 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("--- Cetak KRS ---");
+        System.out.print("Apakah UKT sudah lunas? (True/False): ");
+        boolean uktLunas = sc.nextBoolean();
+        if (uktLunas) {
+            System.out.println("Pembayaran terverifikasi.");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        }
+        sc.close();
+    }
+}
