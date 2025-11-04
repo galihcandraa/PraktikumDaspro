e424472f23f044acad5200f810f6bfec575bd365
galihcandraa <galihcandrakirana17@gmail.com>
2025-11-04 20:40:29 +0700

    Modifikasi Percobaan 3

--- Minggu11/Code/ArrayRataNilai10.java
+++ Minggu11/Code/ArrayRataNilai10.java
@@ -5,21 +5,32 @@ import java.util.Scanner;
 public class ArrayRataNilai10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int mhs = sc.nextInt();
+
+        int[] nilaiMhs = new int[mhs];
+        double totalLulus = 0, totalTdkLulus = 0;
+        double rata2Lulus = 0, rata2TdkLulus = 0;
+        int lulus = 0, tdkLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = sc.nextInt();
+            if (nilaiMhs[i] > 70) {
+                lulus++;
+                totalLulus += nilaiMhs[i];
+            } else {
+                tdkLulus++;
+                totalTdkLulus += nilaiMhs[i];
+            }
         }
 
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
-        }
+        rata2Lulus = totalLulus / lulus;
+        rata2TdkLulus = totalTdkLulus / tdkLulus;
 
-        rata2 = total / nilaiMhs.length;
-        System.out.println("Rata-rata nilai = " + rata2);
+        System.out.println("Rata-rata nilai lulus = " + rata2Lulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rata2TdkLulus);
         sc.close();
     }
 }
