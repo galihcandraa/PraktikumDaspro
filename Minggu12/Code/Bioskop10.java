531e39dcb66c2c1cbf17fb522444ca4a4830b066
galihcandraa <galihcandrakirana17@gmail.com>
2025-11-15 23:13:44 +0700

    Modifikasi Percobaan 1

--- Minggu12/Code/Bioskop10.java
+++ Minggu12/Code/Bioskop10.java
@@ -10,10 +10,19 @@ public class Bioskop10 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hana";
         
         System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
         System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
         System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: " + barisPenonton.length);
+        }
+
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton baris ke-" + (i + 1) + ": " + String.join(", ", penonton[i]));
+        }
     }
 }
