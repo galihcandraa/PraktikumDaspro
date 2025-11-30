c6861d5241cb2ad547b66da088a64ce3a9e7c904
galihcandraa <galihcandrakirana17@gmail.com>
2025-11-30 19:37:50 +0700

    Percobaan 1

---
+++ Minggu14/Code/Kafe10.java
@@ -0,0 +1,18 @@
+package Minggu14.Code;
+
+public class Kafe10 {
+    public static void main(String[] args) {
+        Menu();
+    }
+
+    public static void Menu() {
+        System.out.println("===== MENU RESTO KAFE =====");
+        System.out.println("1. Kopi Hitam - Rp 15.000");
+        System.out.println("2. Cappuccino - Rp 20.000");
+        System.out.println("3. Latte - Rp 22.000");
+        System.out.println("4. Teh Tarik - Rp 12.000");
+        System.out.println("5. Roti Bakar - Rp 10.000");
+        System.out.println("5. Mie Goreng - Rp 18.000");
+        System.out.println("==========================");
+    }
+}
