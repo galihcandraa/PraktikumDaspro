64f85149c79a75690a14650bfed446e2ff781bc1
galihcandraa <galihcandrakirana17@gmail.com>
2025-11-30 20:50:30 +0700

    Percobaan 3

--- Minggu14/Code/Kafe10.java
+++ Minggu14/Code/Kafe10.java
@@ -1,8 +1,17 @@
 package Minggu14.Code;
-
+import java.util.Scanner;
 public class Kafe10 {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         Menu("Budi", true, "DISKON30");
+        System.out.print("\nMasukkan nomor menu yang ingin anda pesan: ");
+        int pilihanMenu = sc.nextInt();
+        System.out.print("Masukkan jumlah item yang ingin anda pesan: ");
+        int banyakItem = sc.nextInt();
+
+        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem);
+
+        System.out.println("Total harga untuk pesanan anda: Rp" + totalHarga);
     }
 
     public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
@@ -27,5 +36,12 @@ public class Kafe10 {
         System.out.println("5. Roti Bakar - Rp 10.000");
         System.out.println("6. Mie Goreng - Rp 18.000");
         System.out.println("==========================");
+    }
+
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem) {
+        int[] hargaItems = { 15000, 20000, 22000, 12000, 10000, 18000 };
+
+        int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+        return hargaTotal;
     }
 }
