6185336fc9bc856f523923c9b9f0d676cac2d22c
galihcandraa <galihcandrakirana17@gmail.com>
2025-12-01 14:02:22 +0700

    Modifikasi Percobaan 4

--- Minggu14/Code/PengunjungCafe10.java
+++ Minggu14/Code/PengunjungCafe10.java
@@ -5,12 +5,13 @@ public class PengunjungCafe10 {
         daftarPengunjung("Ali", "Budi", "Citra");
         daftarPengunjung("Andi");
         daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+        daftarPengunjung();
     }
 
     public static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar nama pengunjung: ");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+        for (String nama : namaPengunjung) {
+            System.out.println("- " + nama);
         }
     }
 }
